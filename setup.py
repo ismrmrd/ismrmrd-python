a38426c48261f79c037c52deae9640c525383038
Kelvin Chow <kelvin.chow@gmail.com>
2022-02-24 23:19:57 -0600

    Bump version to 1.9.7 and revert higher version requirements for h5py

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ def generate_schema(schema_filename, config_filename, outloc  ):
 
 setup(
     name='ismrmrd',
-    version='1.9.6',
+    version='1.9.7',
     author='ISMRMRD Developers',
     author_email='dchansen@gradientsoftware.net',
     description='Python implementation of the ISMRMRD',
@@ -70,7 +70,7 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: Medical Science Apps.'
         ],
-    install_requires=['xsdata==22.2', 'numpy', 'h5py==3.6.0'],
+    install_requires=['xsdata==22.2', 'numpy', 'h5py>=2.3'],
     setup_requires=['nose>=1.0', 'xsdata[cli]==22.2', 'jinja2 >= 2.11'],
     test_suite='nose.collector',
     cmdclass={'build_py':my_build_py}
