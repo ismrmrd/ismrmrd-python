6ded0e83507073ac1bff8f3c9e525019965a6161
Kelvin Chow <kelvin.chow@gmail.com>
2022-06-02 00:04:48 -0500

    Add ImageHeader flag defines for first/last images of each loop counter

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ def generate_schema(schema_filename, config_filename, outloc  ):
 
 setup(
     name='ismrmrd',
-    version='1.10.0',
+    version='1.11.0',
     author='ISMRMRD Developers',
     author_email='dchansen@gradientsoftware.net',
     description='Python implementation of the ISMRMRD',
