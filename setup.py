25def49bd1b4d8dd8789d4df76702266d3dddaf2
V. R <for_usenet@yahoo.com>
2022-08-16 13:34:36 -0400

    Increment version to denote fixes and availability on macOS.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ long_description = (this_directory / "README").read_text()
 
 setup(
     name='ismrmrd',
-    version='1.12.3',
+    version='1.12.4',
     author='ISMRMRD Developers',
     description='Python implementation of the ISMRMRD',
     license='Public Domain',
