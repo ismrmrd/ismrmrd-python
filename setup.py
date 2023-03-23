baf08f29b9f31e56b54b7053d08b0a642a87bed9
David C. Hansen <davidchansen@gradientsoftware.net>
2023-03-23 09:53:04 +0100

    Updated schema

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ long_description = (this_directory / "README").read_text()
 
 setup(
     name='ismrmrd',
-    version='1.13.1',
+    version='1.14.0',
     author='ISMRMRD Developers',
     description='Python implementation of the ISMRMRD',
     license='Public Domain',
